accb36dc8fce3f7b3d2d76fea011079df5da0d79
Kavitha <kavithavaradhan071@gmail.com>
2024-12-03 01:33:59 +0530

    e-commerce project

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>An ecommerce app, also known as a mobile commerce app, 
+            is a piece of software that allows customers to browse and purchase items from an ecommerce store12.
+             Ecommerce apps are beneficial for both business owners and their customers1.
+              Ecommerce mobile apps typically offer features such as search functionality, product recommendations, reviews and ratings,
+               shopping cart management, and order tracking2. Ecommerce applications can be used for retail and wholesale, finance, manufacturing, auctioning,
+                marketing, online shopping, mobile and web applications, and online booking3.
+
+        </p>
+        <p>
+        An ecommerce app, also known as a mobile commerce app, is a piece of software that allows customers to browse and purchase items from an ecommerce store12. Ecommerce apps are beneficial for both business owners and their customers1. Ecommerce mobile apps typically offer features such as search functionality, product recommendations, reviews and ratings, shopping cart management, and order tracking2. Ecommerce applications can be used for retail and wholesale, finance, manufacturing, auctioning, marketing, online shopping, mobile and web applications, and online booking3.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
